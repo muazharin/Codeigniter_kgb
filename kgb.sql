9a861b3ece0b66dca0e72087fb4d3877db5b7ff0
muazharin <muazharin.alfan@studentpartner.com>
2019-02-22 22:57:48 +0800

    database

---
+++ kgb.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2019 at 03:54 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kgb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `duk`
+--
+
+CREATE TABLE `duk` (
+  `id_duk` int(11) NOT NULL,
+  `nip` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `pangkat` varchar(100) NOT NULL,
+  `golongan` varchar(100) NOT NULL,
+  `tmt_pangkat` date NOT NULL,
+  `jabatan` varchar(100) NOT NULL,
+  `tmt_jabatan` date NOT NULL,
+  `masa_kerja_golongan_tahun` int(11) NOT NULL,
+  `masa_kerja_golongan_bulan` int(11) NOT NULL,
+  `masa_kerja_seluruh_tahun` int(11) NOT NULL,
+  `masa_kerja_seluruh_bulan` int(11) NOT NULL,
+  `naik_pangkat_yad` date NOT NULL,
+  `naik_gaji_yad` date NOT NULL,
+  `usia` int(11) NOT NULL,
+  `pendidikan` varchar(50) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id_pegawai` int(11) NOT NULL,
+  `nip` varchar(100) NOT NULL,
+  `jenis_kelamin` varchar(100) NOT NULL,
+  `tempat_lahir` varchar(100) NOT NULL,
+  `umur` int(11) NOT NULL,
+  `golongan_darah` varchar(5) NOT NULL,
+  `agama` varchar(50) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_golongan`
+--
+
+CREATE TABLE `tb_golongan` (
+  `id_golongan` int(11) NOT NULL,
+  `golongan` varchar(100) NOT NULL,
+  `masa_kerja` varchar(100) NOT NULL,
+  `gaji_pokok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `account` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `account`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `duk`
+--
+ALTER TABLE `duk`
+  ADD PRIMARY KEY (`id_duk`);
+
+--
+-- Indexes for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`id_pegawai`);
+
+--
+-- Indexes for table `tb_golongan`
+--
+ALTER TABLE `tb_golongan`
+  ADD PRIMARY KEY (`id_golongan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `duk`
+--
+ALTER TABLE `duk`
+  MODIFY `id_duk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_golongan`
+--
+ALTER TABLE `tb_golongan`
+  MODIFY `id_golongan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
