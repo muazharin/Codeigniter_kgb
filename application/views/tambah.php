e1855fa7fb03eab7b7a8e777394fb9767aa91d83
muazharin <muazharin.alfan@studentpartner.com>
2019-02-24 06:59:20 +0800

    memperbarui halaman tambah

--- application/views/tambah.php
+++ application/views/tambah.php
@@ -21,27 +21,31 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-              <form class="form-horizontal">
+            <?php if(validation_errors()):?>
+              <div class="alert alert-danger" role="alert">
+                <?= validation_errors();?>
+              </div>
+            <?php endif;?>
+              <form class="form-horizontal" method="post" action="">
                   
                   <div class="form-group">
                     <label class="col-md-2 control-label">NIP</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control" required>
+                      <input type="text" name="nip" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Nama</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control" required>
+                      <input type="text" name="nama" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Jenis Kelamin</label>
                     <div class="col-md-8">
-                      <select class="form-control" name="jk" required>
-                        <option value="">-- Pilih --</option>
+                      <select class="form-control" name="jenis_kelamin" required>
                         <option value="Laki-laki">Laki-laki</option>
                         <option value="Perempuan">Perempuan</option>
                       </select>
@@ -51,7 +55,7 @@
                   <div class="form-group">
                     <label class="col-md-2 control-label">Tempat Lahir</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control" required>
+                      <input type="text" name="tempat_lahir" class="form-control" required>
                     </div>
                   </div>
 
@@ -62,23 +66,15 @@
                         <div class="input-group-addon">
                           <i class="fa fa-calendar"></i>
                         </div>
-                        <input type="text" name="tgl" class="form-control pull-right" id="datepicker" required="">
+                        <input type="text" name="tgl_lahir" class="form-control pull-right" id="datepicker" required="">
                       </div>
-                    </div>
-                  </div>
-
-                  <div class="form-group">
-                    <label class="col-md-2 control-label">Umur (Tahun)</label>
-                    <div class="col-md-8">
-                      <input type="number" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Golongan Darah</label>
                     <div class="col-md-8">
-                      <select class="form-control" name="" required>
-                        <option value="">-- Pilih --</option>
+                      <select class="form-control" name="gol_darah" required>
                         <option value="A">A</option>
                         <option value="B">B</option>
                         <option value="AB">AB</option>
@@ -90,35 +86,48 @@
                   <div class="form-group">
                     <label class="col-md-2 control-label">Agama</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control" required>
+                      <select class="form-control" name="agama" required>
+                        <option value="Islam">Islam</option>
+                        <option value="Protestan">Protestan</option>
+                        <option value="Katholik">Katholik</option>
+                        <option value="Hindu">Hindu</option>
+                        <option value="Budha">Budha</option>
+                      </select>
                     </div>
                   </div>
+
+                  <!-- <div class="form-group">
+                    <label class="col-md-2 control-label">Agama</label>
+                    <div class="col-md-8">
+                      <input type="text" name="agama" class="form-control" required>
+                    </div>
+                  </div> -->
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">No. Telepon</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control" required>
+                      <input type="text" name="no_telp" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Email</label>
                     <div class="col-md-8">
-                      <input type="email" class="form-control" required>
+                      <input type="email" name="email" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Alamat</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control" required>
+                      <input type="text" name="alamat" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Keterangan</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control" required>
+                      <input type="text" name="ket" class="form-control" required>
                     </div>
                   </div>
 
