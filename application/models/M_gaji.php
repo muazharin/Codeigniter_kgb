4fba3d003592cfa75da7e21867cf3f4458d92cc1
muazharin <muazharin.alfan@studentpartner.com>
2019-02-24 12:35:29 +0800

    penambahan model baru (M_gaji)

---
+++ application/models/M_gaji.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_Gaji extends CI_Model {
+    public function tambahDataGaji(){
+        $data=[
+            'golongan' => $this->input->post('gol',true),
+            'masa_kerja' => $this->input->post('masa_kerja',true),
+            'gaji_pokok' => $this->input->post('gaji_pokok',true)
+        ];
+        $this->db->insert('tb_golongan', $data);
+    }
+
+    public function getAllDataGaji(){
+        return $this->db->get('tb_golongan')->result_array();
+    }
+
+    public function hapusGajiPegawai($id){
+        $this->db->where('id_golongan', $id);
+        $this->db->delete('tb_golongan');
+    }
+}
