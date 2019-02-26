f34ffcf20f52e9a4cb9db0e4d7887ee9b9aa5682
muazharin <muazharin.alfan@studentpartner.com>
2019-02-26 21:17:04 +0800

    menambahkan helper nominal dan tgl serta model user

---
+++ application/models/M_user.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_user extends CI_Model {
+    public function dataClient(){
+        $id=$this->session->userdata('username');
+        return $this->db->get_where('duk', ['nip'=>$id])->row_array();
+    }
+    public function getPegawaiById($id){
+        return $this->db->get_where('pegawai', ['nip'=>$id])->row_array();
+    }
+    public function ubahDataPegawai(){
+        $dataPegawai = [
+            'nip'=>$this->input->post('nip',true),
+            'nama'=>$this->input->post('nama',true),
+            'jenis_kelamin'=>$this->input->post('jenis_kelamin',true),
+            'tempat_lahir'=>$this->input->post('tempat_lahir',true),
+            'tgl_lahir'=>$this->input->post('tgl_lahir',true),
+            'umur'=>'0',
+            'golongan_darah'=>$this->input->post('gol_darah',true),
+            'agama'=>$this->input->post('agama',true),
+            'no_telp'=>$this->input->post('no_telp',true),
+            'email'=>$this->input->post('email',true),
+            'alamat'=>$this->input->post('alamat',true),
+            'keterangan'=>$this->input->post('ket',true)
+        ];
+        $this->db->where('id_pegawai',$this->input->post('id'));
+        $this->db->update('pegawai',$dataPegawai);
+
+        $dataDuk = [
+            'nip'=>$this->input->post('nip',true),
+            'nama'=>$this->input->post('nama',true),
+            'keterangan'=>$this->input->post('ket',true)
+        ];
+        $this->db->where('id_duk',$this->input->post('id'));
+        $this->db->update('duk',$dataDuk);
+
+        $dataUser = [
+            'username'=>$this->input->post('nip',true),
+            'password'=>md5($this->input->post('nip',true)),
+            'account' => 'pegawai'
+        ];
+        $this->db->where('username',$this->input->post('nip'));
+        $this->db->update('user',$dataUser);
+
+    }
+}
