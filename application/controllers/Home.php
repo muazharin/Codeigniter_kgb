a8d9e6e39981243a9ce5c0dcbd2f46d4d1cd07c7
muazharin <muazharin.alfan@studentpartner.com>
2019-03-03 15:58:33 +0800

    update controller home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,15 +6,17 @@ class Home extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
-        if($this->session->userdata('logged_in') !== TRUE){
+        if($this->session->userdata('logged_in_admin') !== TRUE){
             redirect('login');
         }
     }
 	public function index()
 	{
+        $data['tot_pgw']=$this->db->count_all_results('pegawai');
+        $data['tot_usr']=$this->db->count_all_results('user');
 		$data['sidebar']="#mn1";
         $this->load->view('header');
-        $this->load->view('home');
+        $this->load->view('home', $data);
 		$this->load->view('footer', $data);
 	}
 }
