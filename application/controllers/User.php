8bd9938f54fe2ffb1f3dbfa7665cf228bc9fc7a0
muazharin <muazharin.alfan@studentpartner.com>
2019-03-03 15:59:28 +0800

    update controller user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,7 +5,7 @@ class User extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
-        if($this->session->userdata('logged_in') !== TRUE){
+        if($this->session->userdata('logged_in_user') !== TRUE){
             redirect('login');
         }
         $this->load->model('M_user');
