8f8c1d3571850fb8660214557a36f185c504c532
muazharin <muazharin.alfan@studentpartner.com>
2019-03-13 00:47:46 +0800

    update tampilan edit gaji

--- application/views/edit-gaji.php
+++ application/views/edit-gaji.php
@@ -5,6 +5,7 @@
       <h1>
         Edit Data
         <small>Gaji Pegawai</small>
+        <a href="<?= base_url() ?>gaji" class="btn btn-warning" style="float: right;"> <i class="fa fa-angle-double-left"></i> Kembali</a>
       </h1>
       <ol class="breadcrumb">
        
@@ -18,29 +19,37 @@
           <div class="box box-success">
             <div class="box-header">
               <h3 class="box-title"><i class="fa fa-plus"></i> Edit Data Gaji Pegawai</h3>
-              <a href="<?= base_url() ?>gaji" class="btn btn-warning" style="float: right;"> <i class="fa fa-angle-double-left"></i> Kembali</a>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
               <form class="form-horizontal" method="post" action="">
-                  <div class="form-group">
-                    <label class="col-md-2 control-label">Golongan</label>
+                <input type="hidden" name="id" value="<?= $gaji['id_golongan'];?>">
+                <div class="form-group">
+                  <label class="col-md-2 control-label">Golongan</label>
                     <div class="col-md-8">
-                      <input type="text" name="gol" class="form-control" required>
+                      <select class="form-control" name="gol" required>
+                        <?php foreach($gol as $a):?>
+                          <?php if($a == $gaji['golongan'] ):?>
+                              <option value="<?= $a;?>" selected><?= $a;?></option>
+                            <?php else :?>
+                              <option value="<?= $a;?>"><?= $a;?></option>
+                            <?php endif;?>
+                        <?php endforeach;?>
+                      </select>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Masa Kerja</label>
                     <div class="col-md-8">
-                      <input type="text" name="masa_kerja" class="form-control" required>
+                      <input type="number" name="masa_kerja" value="<?=$gaji['masa_kerja'];?>" class="form-control" required>
                     </div>
                   </div>
 
                   <div class="form-group">
                     <label class="col-md-2 control-label">Gaji Pokok</label>
                     <div class="col-md-8">
-                      <input type="number" name="gaji_pokok" class="form-control" required>
+                      <input type="number" name="gaji_pokok" value="<?=$gaji['gaji_pokok'];?>" class="form-control" required>
                     </div>
                   </div>
 
