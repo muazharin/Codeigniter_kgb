cbb2070b0b22ad170bdec493a195859cb61fc4d6
muazharin <muazharin.alfan@studentpartner.com>
2019-03-13 00:46:08 +0800

    update model M_gaji

--- application/models/M_gaji.php
+++ application/models/M_gaji.php
@@ -15,6 +15,20 @@ class M_Gaji extends CI_Model {
     public function getAllDataGaji(){
         return $this->db->get('tb_golongan')->result_array();
     }
+    
+    public function getDataGajiById($id){
+        return $this->db->get_where('tb_golongan', ['id_golongan'=>$id])->row_array();
+    }
+
+    public function updateDataGaji(){
+        $data=[
+            'golongan' => $this->input->post('gol',true),
+            'masa_kerja' => $this->input->post('masa_kerja',true),
+            'gaji_pokok' => $this->input->post('gaji_pokok',true)
+        ];
+        $this->db->where('id_golongan', $this->input->post('id'), true);
+        $this->db->update('tb_golongan',$data);
+    }
 
     public function hapusGajiPegawai($id){
         $this->db->where('id_golongan', $id);
